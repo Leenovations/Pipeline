07923668570ce9a560da98976087b70aa431b00f
ltk <xorrb85@gmail.com>
2024-02-06 09:22:13 +0900

    Update

---
+++ ChromosomalCNV.R
@@ -0,0 +1,79 @@
+library(gplots)
+library(RColorBrewer)
+library(dplyr)
+library(ggplot2)
+library(ggbiplot)
+library(ggrepel)
+#------------------------------------------------------------------------#
+args <- commandArgs(trailingOnly = TRUE)
+Sample <- args[1]
+Path <- getwd()
+#-----------------------------------------------------------------------------------#
+Batch <- read.table(paste0(Path, '/', Sample, '.batch.config'), sep='\t', col.names='Option')
+Type <- strsplit(Batch[1,1], '=')[[1]][2]
+
+if (Type=='WGBS'){
+  Data <- read.table(paste0(Path, '/04.ChromosomeCNV/', sprintf('%s', Sample), '.Chromosome.CNV.txt'),
+                     sep='\t',
+                     header=T)
+} else if (Type=='WGS'){
+  Data <- read.table(paste0(Path, '/05.SV', '/00.ChromosomeCNV/', sprintf('%s', Sample), '.Chromosome.CNV.txt'),
+                       sep='\t',
+                       header=T)
+}
+#-----------------------------------------------------------------------------------#
+Chromosome <- unique(Data$Chr)
+max_values <- c()
+median_values <- c()
+#-----------------------------------------------------------------------------------#
+for (chr in Chromosome) {
+  Sub <- subset(Data, Chr == chr)
+  max_values <- c(max_values, max(Sub$Order))
+  median_values <- c(median_values, median(Sub$Order))
+}
+max_values <- max_values[1:length(max_values)-1]
+#-----------------------------------------------------------------------------------#
+labels <- Chromosome
+data <- data.frame(x = median_values, y = 1)
+#-----------------------------------------------------------------------------------#
+CNV <- ggplot(Data, aes(x=Order, y=Norm)) +
+  geom_point(size=0.5) +
+  ggtitle('Chromosomal copy numbers\n', subtitle = sprintf('%s', Sample)) + 
+  xlab('') +
+  ylab('Normalized Depth') +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_continuous(breaks = c(0), labels=c(0)) +
+  geom_hline(yintercept=0,
+             linetype='dashed',
+            color='red',
+            alpha=0.5) + 
+  geom_vline(xintercept = max_values, 
+             linetype = "solid", 
+             color = "gray", 
+             alpha=0.5) +
+  annotate(geom = "text", x = median_values, y = -0.001, label = unique(Data$Chr), size = 4.5) +
+  coord_cartesian(ylim = c(-0.0003, 0.005), expand = T, clip = "off") +
+  theme_bw() +
+  theme(plot.margin = unit(c(2,2,5,2), "lines"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=15, face='bold'),
+        plot.title = element_text(size=20, hjust=0.5, face = 'bold'),
+        plot.subtitle = element_text(size=10, hjust=0.5),
+        axis.title = element_text(size = 10),
+        axis.text=element_text(color="black"),
+        axis.ticks.x=element_blank(),
+        axis.text.x=element_blank())
+#-----------------------------------------------------------------------------------#
+if (Type=='WGBS'){
+  ggsave(paste0(Path, '/04.ChromosomeCNV/', sprintf('%s', Sample), '.Chromosome.pdf'),
+         plot=CNV,
+         width=15)
+  ggsave(paste0(Path, '/04.ChromosomeCNV/', sprintf('%s', Sample), '.Chromosome.png'),
+         plot=CNV,
+         width=15)
+} else if (Type=='WGS'){
+  ggsave(paste0(Path, '/05.SV/', sprintf('%s', Sample), '.Chromosome.pdf'),
+         plot=CNV,
+         width=15)
+}
---
+++ GeneCNVConcat.py
@@ -0,0 +1,54 @@
+#!/home/lab/anaconda3/envs/NGS/bin/python3
+
+import pandas as pd
+import glob
+import numpy as np
+#--------------------------------------------------#
+def Merge(Name):
+    Exon = pd.read_csv(f"/media/src/hg19/08.bed/NCBI.RefSeq.Selected.GeneCNV.Chr.X.bed",
+                       sep='\t',
+                       low_memory=False,
+                       header=None)
+    Gene = list(set(Exon.iloc[:, 3].to_list()))
+    Gene.sort()
+
+    CNV = glob.glob("../Intermediate/*txt")
+    CNV.sort()
+
+    DATA = None
+    for data in CNV:
+        Sample = data.split('/')[-1]
+        Sample = Sample.split('.')[0]
+        Data = pd.read_csv(data,
+                        sep='\t',
+                        header=None,
+                        names=['Chr', 'Start', 'End', 'Gene', 'Strand', f'{Sample}'])
+        
+        if DATA is None:
+            DATA = Data
+        else:
+            DATA = pd.merge(DATA, Data, on=['Chr', 'Start', 'End', 'Gene', 'Strand'])
+
+    Info = DATA.iloc[:, :5]
+    Coverage = DATA.iloc[:, 5:]
+
+    Exon_length = DATA['End'] - DATA['Start']
+    Depth_Length = Coverage.apply(lambda count: (count / Exon_length))
+    Sum_Depth_Length = Depth_Length.sum(axis=0)
+
+    Exon_length = np.array(Exon_length)
+    Sum_Depth_Length = np.array(Sum_Depth_Length)
+    result_matrix = np.outer(Exon_length, Sum_Depth_Length)
+    NormFactor = pd.DataFrame(result_matrix)
+    NormFactor.columns = list(Coverage.columns)
+    NormFactor.index = DATA.index.to_list()
+    CNV_ALL = Coverage.div(NormFactor)
+    CNV_ALL = CNV_ALL + 1
+    CNV_ALL = CNV_ALL.applymap(np.log10)
+    CNV_ALL = CNV_ALL.apply(lambda value : value - CNV_ALL.mean(axis=1))
+    DATA = pd.concat([Info, CNV_ALL], axis=1)
+    DATA = DATA[['Chr', 'Start', 'End', 'Gene', f'{Name}']]
+    DATA.to_csv(f"05.SV/01.GeneCNV/{Name}.Norm.CNV.txt",
+                sep='\t',
+                index=False,
+                header='infer')
